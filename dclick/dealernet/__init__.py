f017ef772fe1b96627caaa574f186cba18b0695e
Alex Angelo <alex.angelo@dclick.com.br>
2025-05-06 16:25:08 -0300

    Adicionado o export do cadastro em pacote dealernet

--- dclick/dealernet/__init__.py
+++ dclick/dealernet/__init__.py
@@ -1,4 +1,4 @@
 """Pacote destinado ao `Sistema Web Dealer-Net`"""
 
 from dclick.dealernet.login import *
-from dclick.dealernet import menus, integracao, produtos, financeiro
+from dclick.dealernet import menus, integracao, produtos, financeiro, cadastro
