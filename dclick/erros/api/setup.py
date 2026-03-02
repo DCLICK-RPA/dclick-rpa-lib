bb1758ec1d2f382310eea708d1b860f48f569961
Alex Angelo <alex.angelo@dclick.com.br>
2026-03-02 15:13:16 -0300

    Refatoramento

--- dclick/erros/api/setup.py
+++ dclick/erros/api/setup.py
@@ -9,10 +9,10 @@ Timeout = ModeloErroComLog("API.TIMEOUT.032", CATEGORIA, "Tempo de resposta exce
 Conexao = ModeloErroComLog("API.CONN.035", CATEGORIA, "Falha de conexão com endpoint da API", "Alta", "TI", "API")
 """Falha de conexão com endpoint da API"""
 
-Autenticacao =ModeloErroComLog("API.AUTH.031", CATEGORIA, "Erro de autenticação na API", "Crítica", "TI/Sistema", "API")
+Autenticacao = ModeloErroComLog("API.AUTH.031", CATEGORIA, "Erro de autenticação na API", "Crítica", "TI/Sistema", "API")
 """Erro de autenticação na API"""
 
-RespostaJson =ModeloErroComLog("API.JSON.034", CATEGORIA, "Erro ao processar JSON de resposta da API", "Alta", "Bot", "API")
+RespostaJson = ModeloErroComLog("API.JSON.034", CATEGORIA, "Erro ao processar JSON de resposta da API", "Alta", "Bot", "API")
 """Erro ao processar JSON de resposta da API"""
 
 RetornoInesperado = ModeloErroComLog("API.RET.033", CATEGORIA, "Retorno inesperado da API", "Alta", "Sistema", "API")
