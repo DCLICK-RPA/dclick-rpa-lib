5337fa876739f6fa6473405a1399ea9c21cb7846
Alex Angelo <alex.angelo@dclick.com.br>
2026-03-09 14:21:21 -0300

    Adicionado dados e descrição ao logger

--- dclick/logger/setup.py
+++ dclick/logger/setup.py
@@ -1,9 +1,10 @@
 # externo
-from bot.logger.interfaces import MainLogger
+from bot.logger.interfaces import MainLogger, TracerLogger
 
 logger = MainLogger("DCLICK")
 """Classe pré-configurada para criar, consultar e tratar os arquivos de log
 - Logger de `name: DCLICK`
+- Utilizar `dclick.logger.obter_logger(nome)` ou importar o `MainLogger(nome)` para criar uma instância com outro nome
 
 #### Inicializar manualmente `logger.inicializar_logger()` pela lib da `dclick` ou `bot` para inicializar os handlers
 - Stream para o `stdout`
