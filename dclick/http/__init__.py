1e1f35a3047790a729b143eac9054a209d35f338
Alex Angelo <alex.angelo@dclick.com.br>
2026-03-09 14:22:51 -0300

    Correção descrição

--- dclick/http/__init__.py
+++ dclick/http/__init__.py
@@ -1,4 +1,4 @@
 """Pacote destinado ao protocolo http
-- Realizado logs de `erros.api` automáticamente para o `request e response` dos métodos novos/modificados"""
+- Realizado logs de `erros.api` automaticamente para o `request e response` dos métodos novos/modificados"""
 
 from dclick.http.setup import *
